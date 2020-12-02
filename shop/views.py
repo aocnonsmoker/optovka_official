f1c8dfcd937efab82efa531634022d587079b5d9
Zhambyl Kairken <z.kairken@csi.kz>
2020-12-02 15:30:47 +0600

    new interface re-designing

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,4 @@
+import random
 from django.shortcuts import render
 from .models import Category, Brand, Product
 from cart.forms import CartAddProductForm
@@ -5,6 +6,8 @@ from cart.forms import CartAddProductForm
 def shopPage(request, category_slug=None):
     context = {}
     categories = Category.objects.all()
+    products = sorted(Product.objects.all(), key=lambda x: random.random())
+    context['products'] = products
     context['categories'] = categories
     return render(request, 'shop/shop.html', context)
 
@@ -17,7 +20,21 @@ def brand_list(request, slug):
     category = Category.objects.get(slug=slug)
     brands = Brand.objects.filter(category=category)
     context['category'] = category
-    context['brands'] = brands
+    brand_arr = []
+    product_all = []
+    for brand in brands:
+        product_len = len(Product.objects.filter(brand=brand))
+        b = {
+            'name': brand.name,
+            'slug': brand.slug,
+            'products_len': product_len
+        }
+        brand_arr.append(b)
+        for prod in Product.objects.filter(brand=brand):
+            product_all.append(prod)
+    context['brands'] = brand_arr
+    context['product_all'] = product_all
+    print(product_all)
     return render(request, 'shop/list.html', context)
 
 def products_list(request, slug):
@@ -25,6 +42,7 @@ def products_list(request, slug):
     brand = Brand.objects.get(slug=slug)
     products = Product.objects.filter(brand=brand)
     context['products'] = products
+    context['brand'] = brand
     return render(request, 'shop/products.html', context)
 
 def product_detail(request, id):
